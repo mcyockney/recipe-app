e3ee13d261849afb82631128c4e5bdfa85c2c055
Matt <matthewy@1kiwi.com>
2015-11-27 10:39:09 +0000

    Building the ingredients and style models plus associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123191316) do
+ActiveRecord::Schema.define(version: 20151127101232) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -21,12 +21,26 @@ ActiveRecord::Schema.define(version: 20151123191316) do
     t.string   "password_digest"
   end
 
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
+
   create_table "likes", force: :cascade do |t|
     t.boolean  "like"
     t.integer  "chef_id"
     t.integer  "recipe_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "ingredient_id"
+    t.integer "recipe_id"
+  end
+
+  create_table "recipe_styles", force: :cascade do |t|
+    t.integer "style_id"
+    t.integer "recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -40,5 +54,9 @@ ActiveRecord::Schema.define(version: 20151123191316) do
   end
 
   add_index "recipes", ["chef_id"], name: "index_recipes_on_chef_id"
+
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
